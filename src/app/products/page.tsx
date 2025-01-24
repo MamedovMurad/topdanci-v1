6a38c5c87c26b56f8b6ec859c805293f45feedef
MamedovMurad <muradvm@code.edu.az>
2025-01-24 08:07:38 -0800

    added responsive for products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -9,12 +9,16 @@ interface ProductsPageProps {
  
 const ProductsPage: FunctionComponent<ProductsPageProps> = () => {
     return (   <main>
+        <section className=" hidden md:block">
         <Banner isOpen={false} />
+        </section>
   
         <section >
-          <div className="container mx-auto mt-5  ">
-            <Filter/>
-            <div className=" mb-20 mt-5  ">
+          <div className="container mx-auto md:mt-5  ">
+           <div className=" md:block hidden">
+           <Filter/>
+           </div>
+            <div className=" mb-20 md:mt-5   ">
              
     
         <ProductsContainer/>
